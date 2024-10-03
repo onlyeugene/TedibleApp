9ffbb93273d344db49e037240ad904c6690684e7
onlyeugene <102293184+onlyeugene@users.noreply.github.com>
2024-10-03 18:25:08 +0100

    feat: successfully moved the external from React to Nextjs and added new designs

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -1,5 +1,5 @@
 import Section from '@/components/external/hero/homeHero';
-import React, { ReactNode } from 'react';
+import React from 'react';
 import SpecialMenu from './specialMenu';
 import Slider from '@/components/ui/slider';
 import Features from './features';
