0f8358592031eac5d5c3318f148c5c29be318db8
onlyeugene <102293184+onlyeugene@users.noreply.github.com>
2024-10-06 21:59:28 +0100

    feat: added the google authentication functionality

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
+import nextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const handler = nextAuth ({
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID!,
@@ -9,8 +9,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-};
+});
 
-export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
