aac038a595f29bef0ce1a4c1cac3548b38c11d84
onlyeugene <102293184+onlyeugene@users.noreply.github.com>
2024-10-07 14:45:15 +0100

    feat: developed the notifications page and added a lib const

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,34 @@
 import "./globals.css";
 import Navbar from "@/components/external/navbar";
 import Footer from "@/components/external/footer";
+// import { usePathname } from "next/navigation"; // Use usePathname instead of useRouter for server components
+
+
 import { getServerSession } from "next-auth";
-import { NextAuthProvider } from "@/utils/NextAuthProvider";
+import AuthProvider from "@/utils/AuthProvider";
+
+
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession();
+  // const pathname = usePathname(); // This helps check the current route
+  // const showFooter = !["/login", "/register"].includes(pathname); // Exclude footer on certain pages
+
+  const session = await getServerSession()
 
   return (
     <html lang="en">
       <body>
-        <NextAuthProvider session={session}>
-          <Navbar />
-          {children}
-          <Footer />
-        </NextAuthProvider>
+        <AuthProvider session={session}>
+        <Navbar />
+        {children}
+        {/* {showFooter && */}
+        <Footer />
+        {/* // } Conditionally render the footer */}
+        </AuthProvider>
       </body>
     </html>
   );
