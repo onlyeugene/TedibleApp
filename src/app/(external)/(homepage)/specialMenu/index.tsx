b245ecf15019926cd5f5b3f45d0eba1fb5e25ab9
Chioma <okekelinda9991@gmail.com>
2024-10-30 00:07:17 -0700

    worked on menu card

--- src/app/(external)/(homepage)/specialMenu/index.tsx
+++ src/app/(external)/(homepage)/specialMenu/index.tsx
@@ -1,98 +1,30 @@
-'use client'
-
-
-import React, {useState } from "react";
-// import axiosInstance from "../../../utils/axiosInstance";
-
-
-// interface Product {
-//   id: number;
-//   itemName: string;
-//   itemImage: string;
-//   price: number;
-// }
+import React, { useState } from "react";
+import menu1 from '@/assets/home/specialmenu/menu 1.svg'
+import Page from '@/components/cards/menucard/index'
+import Image from "next/image";
+import stars from "@/assets/home/specialmenu/five stars.svg";
+import plus from "@/assets/home/specialmenu/plus icon.svg";
 
 const SpecialMenu: React.FC = () => {
-  // const [products, setProducts] = useState<Product[]>([]);
-  // const [error, setError] = useState<string | null>(null);
-  const [showMore, setShowMore] = useState(false);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       // const response = await axiosInstance.get("/api/product");
-  //       // const productData: Product[] = response.data;
-  //       // setProducts(productData);
-  //     } catch (error) {
-  //       setError("Oops! Something went wrong, please refresh.");
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
-  const handleSeeMore = () => {
-    setShowMore((prevShowMore) => !prevShowMore);
-  };
-
-  // const displayedProducts = showMore ? products : products.slice(0, 4);
-
   return (
-    <div>
-      <div className="w-full py-4">
-        <div className="text-center">
-          <h1 className="sm:text-[50px] text-[29px] text text-center font-medium">Special Menu</h1>
-          <p className="sm:text-lg text-center text-sm">
-            Discover our specially curated menu, designed to <br className="sm:hidden block"/> delight your taste
-            buds <br className="sm:block hidden"/> with unique and seasonal <br className="sm:hidden block"/> dishes
-          </p>
-        </div>
-        {/* <div className="categoryproducts">
-          {displayedProducts.map((product) => (
-            <div key={product.id}>
-              <div className="firstproduct">
-                <Image
-                  className="productsimg"
-                  src={product.itemImage}
-                  alt={product.itemName}
-                />
-                <div className="innerproducts">
-                  <small>Ratings</small>
-                  <p>{product.itemName}</p>
-                  <small>Mcdonalds</small>
-                  <div className="productprice">
-                    <p className="cartprice">{product.price}</p>
-                    <p className="plussign">+</p>
-                  </div>
-                </div>
-              </div>
-              {showMore && (
-                <div className="secondproduct">
-                  <Image
-                    className="productsimg"
-                    src={product.itemImage}
-                    alt={product.itemName}
-                    width={500}
-                    height={500}
-                  />
-                  <div className="innerproducts">
-                    <small>Ratings</small>
-                    <p>{product.itemName}</p>
-                    <small>Mcdonalds</small>
-                    <div className="productprice">
-                      <p className="cartprice">{product.price}</p>
-                      <p className="plussign">+</p>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          ))}
-        </div> */}
-        <p onClick={handleSeeMore} className="seemorecart">
-          {showMore ? "Show Less" : "See More"}
-        </p>
+    <section className="relative py-4 container mx-auto px-5">
+      <div className="">
+        <h1 className="sm:text-[50px] text-[29px] text font-medium text-center">
+          Special Menu
+        </h1>
+        <h5 className="text-sm md:text-base lg:text-lg 2xl:text-xl text-center">
+          Discover our specially curated menu, designed to delight your taste
+          buds with unique and seasonal dishes
+        </h5>
+
+        {/* Card */}
+        <Page 
+        image={menu1}
+        name="Chicken & Chips"
+        restaurant="Macdonalds"
+        price="N2500"/>
       </div>
-    </div>
+    </section>
   );
 };
 
