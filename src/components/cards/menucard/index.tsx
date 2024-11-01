181d369f11f3374dfb898e5de86a05499506d6c3
Chioma <okekelinda9991@gmail.com>
2024-10-31 19:54:22 -0700

    refactoring Specialmenu.tsx

--- src/components/cards/menucard/index.tsx
+++ src/components/cards/menucard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import stars from "@/assets/home/specialmenu/five stars.svg";
 import plus from "@/assets/home/specialmenu/plus icon.svg";
 import Image from "next/image";
@@ -14,34 +14,37 @@ interface PageProps {
 // Use the PageProps interface as the type for the component props
 const Page: React.FC<PageProps> = ({ image, name, restaurant, price }) => {
   return (
-    
-      <div className=" -mt-1 relative bg-white shadow-[0px_0px_12px_8px_rgba(0,0,0,0.03)] rounded-xl">
-        {/* Image Section */}
-        <div>
-          <Image
-            src={image}
-            alt="Food item"
-            className="w-full h-full"
-            width={200}
-            height={200}
-            objectFit="cover"
-          />
+    <div className="flex flex-col gap-[21px] max-w-[27.727rem] bg-white shadow-[0px_4px_38px_0px_rgba(0,0,0,0.25)] rounded-[2.188rem]">
+      {/* Image Section */}
+      <div>
+        <Image
+          src={image}
+          alt="Food item"
+          className="w-[27.727rem] -mt-8 transition-transform duration-300 ease-in-out transform cursor-pointer"
+          height={200}
+        />
+      </div>
+
+      {/* Content Section */}
+      <div className="px-[1.3rem] pb-[1.5rem]">
+        <Image src={stars} alt="Five-star rating" className=""/>
+        <div className="text-[.5rem] lg:text-[1.5rem] mt-2">
+          <p className="z-20 text-black text-sm lg:text-base 2xl:text-lg font-semibold">
+            {name}
+          </p>
+          <p className="text-black text-xs lg:text-sm 2xl:text-base font-light">
+            {restaurant}
+          </p>
         </div>
 
-        {/* Content Section */}
-        <div className="p-[1.5rem]">
-          <Image src={stars} alt="Five-star rating" width={100} height={20} />
-          <div className="text-[.5rem] lg:text-[1.5rem] mt-2">
-            <p className="z-20 text-black font-bold text-sm md:text-base">{name}</p>
-            <p className="text-gray-600 text-xs md:text-sm">{restaurant}</p>
-          </div>
-
-          <div className="flex justify-between items-center mt-4">
-            <p className="text-lg lg:text-2xl font-semibold text-orange-500">N{price}</p>
-            <Image src={plus} alt="Add to cart icon" className="w-4 md:w-6" width={24} height={24} />
-          </div>
+        <div className="flex justify-between items-center">
+          <p className="text-[0.839rem] lg:text-[1.3rem] font-semibold text-tertiary">
+            N{price}
+          </p>
+          <button className="bg-tertiary p-[.3rem] lg:p-[.625rem] rounded-full hover:bg-orange-400"><Image src={plus} alt="Add to Cart icon"/></button>
         </div>
       </div>
+    </div>
   );
 };
 
