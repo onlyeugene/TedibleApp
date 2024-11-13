78cc6511d5831feb7e0e10a86ea799094942e3b3
Chioma <okekelinda9991@gmail.com>
2024-11-13 00:22:15 -0800

    menu card

--- src/app/(external)/(homepage)/specialMenu/index.tsx
+++ src/app/(external)/(homepage)/specialMenu/index.tsx
@@ -1,77 +1,9 @@
 import React from "react";
-import menu1 from "@/assets/home/specialmenu/menu 1.svg";
-import menu2 from "@/assets/home/specialmenu/menu 2.svg";
-import menu3 from "@/assets/home/specialmenu/menu 3.svg";
-import menu4 from "@/assets/home/specialmenu/menu 4.svg";
-import menu5 from "@/assets/home/specialmenu/menu 5.svg";
-import menu6 from "@/assets/home/specialmenu/menu 6.svg";
-import menu7 from "@/assets/home/specialmenu/menu 7.svg";
-import menu8 from "@/assets/home/specialmenu/menu 8.svg";
 import arrowright from "@/assets/home/specialmenu/arrowright.svg";
-import Page from "@/components/cards/menucard/index";
 import Image from "next/image";
 import Link from "next/link";
 import MenuCard from "@/components/cards/menucard/index";
 import { Top_Order } from "@/lib/consts/top-order";
-
-// const menu = [
-//   {
-//     id: 1,
-//     image: menu1,
-//     price: "2500",
-//     name: "Spicy food",
-//     restaurant: "Spice Route",
-//   },
-//   {
-//     id: 2,
-//     image: menu2,
-//     price: "3000",
-//     name: "Chicken & chips",
-//     restaurant: "Kenturkey Fried Chicken",
-//   },
-//   {
-//     id: 3,
-//     image: menu3,
-//     price: "2000",
-//     name: "Beef",
-//     restaurant: "Chicken Republic",
-//   },
-//   {
-//     id: 4,
-//     image: menu4,
-//     price: "1500",
-//     name: "Vegetable",
-//     restaurant: "Awoof Brekete",
-//   },
-//   {
-//     id: 5,
-//     image: menu5,
-//     price: "1500",
-//     name: "Vegetable",
-//     restaurant: "Awoof Brekete",
-//   },
-//   {
-//     id: 6,
-//     image: menu6,
-//     price: "1500",
-//     name: "Vegetable",
-//     restaurant: "Awoof Brekete",
-//   },
-//   {
-//     id: 7,
-//     image: menu7,
-//     price: "1500",
-//     name: "Vegetable",
-//     restaurant: "Awoof Brekete",
-//   },
-//   {
-//     id: 8,
-//     image: menu8,
-//     price: "2500",
-//     name: "Vegetable",
-//     restaurant: "Awoof Brekete",
-//   },
-// ];
 
 // Page Rendering
 const SpecialMenu: React.FC = () => {
