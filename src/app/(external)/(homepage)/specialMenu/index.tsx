904e6bd645e7019d3073b9e70ad7c12c78320496
Chioma <okekelinda9991@gmail.com>
2024-11-13 00:18:17 -0800

    menu card

--- src/app/(external)/(homepage)/specialMenu/index.tsx
+++ src/app/(external)/(homepage)/specialMenu/index.tsx
@@ -11,70 +11,72 @@ import arrowright from "@/assets/home/specialmenu/arrowright.svg";
 import Page from "@/components/cards/menucard/index";
 import Image from "next/image";
 import Link from "next/link";
+import MenuCard from "@/components/cards/menucard/index";
+import { Top_Order } from "@/lib/consts/top-order";
 
-const menu = [
-  {
-    id: 1,
-    image: menu1,
-    price: "2500",
-    name: "Spicy food",
-    restaurant: "Spice Route",
-  },
-  {
-    id: 2,
-    image: menu2,
-    price: "3000",
-    name: "Chicken & chips",
-    restaurant: "Kenturkey Fried Chicken",
-  },
-  {
-    id: 3,
-    image: menu3,
-    price: "2000",
-    name: "Beef",
-    restaurant: "Chicken Republic",
-  },
-  {
-    id: 4,
-    image: menu4,
-    price: "1500",
-    name: "Vegetable",
-    restaurant: "Awoof Brekete",
-  },
-  {
-    id: 5,
-    image: menu5,
-    price: "1500",
-    name: "Vegetable",
-    restaurant: "Awoof Brekete",
-  },
-  {
-    id: 6,
-    image: menu6,
-    price: "1500",
-    name: "Vegetable",
-    restaurant: "Awoof Brekete",
-  },
-  {
-    id: 7,
-    image: menu7,
-    price: "1500",
-    name: "Vegetable",
-    restaurant: "Awoof Brekete",
-  },
-  {
-    id: 8,
-    image: menu8,
-    price: "2500",
-    name: "Vegetable",
-    restaurant: "Awoof Brekete",
-  },
-];
+// const menu = [
+//   {
+//     id: 1,
+//     image: menu1,
+//     price: "2500",
+//     name: "Spicy food",
+//     restaurant: "Spice Route",
+//   },
+//   {
+//     id: 2,
+//     image: menu2,
+//     price: "3000",
+//     name: "Chicken & chips",
+//     restaurant: "Kenturkey Fried Chicken",
+//   },
+//   {
+//     id: 3,
+//     image: menu3,
+//     price: "2000",
+//     name: "Beef",
+//     restaurant: "Chicken Republic",
+//   },
+//   {
+//     id: 4,
+//     image: menu4,
+//     price: "1500",
+//     name: "Vegetable",
+//     restaurant: "Awoof Brekete",
+//   },
+//   {
+//     id: 5,
+//     image: menu5,
+//     price: "1500",
+//     name: "Vegetable",
+//     restaurant: "Awoof Brekete",
+//   },
+//   {
+//     id: 6,
+//     image: menu6,
+//     price: "1500",
+//     name: "Vegetable",
+//     restaurant: "Awoof Brekete",
+//   },
+//   {
+//     id: 7,
+//     image: menu7,
+//     price: "1500",
+//     name: "Vegetable",
+//     restaurant: "Awoof Brekete",
+//   },
+//   {
+//     id: 8,
+//     image: menu8,
+//     price: "2500",
+//     name: "Vegetable",
+//     restaurant: "Awoof Brekete",
+//   },
+// ];
 
 // Page Rendering
 const SpecialMenu: React.FC = () => {
   return (
-    <section className="relative mt-[.5rem] mb-[3.25rem] lg:my-[3.25rem] container w-11/12">
+    <section className="mt-[.5rem] mb-[3.25rem] lg:my-[3.25rem] container w-11/12">
       <div className="grid place-content-center pb-[60px]">
         <h1 className="sm:text-[50px] text-[29px] text font-medium text-center">
           Special Menu
@@ -84,17 +86,17 @@ const SpecialMenu: React.FC = () => {
           buds with unique and seasonal dishes
         </h5>
       </div>
-
-      <div className="scrollbar-hide overflow-x-auto lg:overflow-x-visible scrollbar-y-[red-200]">
+      <div className="w-full scrollbar-hide overflow-x-auto lg:overflow-x-clip">
+  <div className="flex lg:grid lg:grid-cols-4 gap-4 lg:gap-8">
+    {Top_Order.map((item) => (
+      <div key={item.id} className="w-[50%] md:w-[calc(30%-0.594rem)] lg:w-auto">
         {/* Menu Card */}
-        <div className="flex items-center lg:grid lg:grid-cols-4 gap-[1.188rem] lg:gap-[3.198rem] lg:gap-y-[100px]">
-          {menu.map((item) => (
-            <div key={item.id} className="flex-none w-[50%] md:w-[calc(30%-0.594rem)] lg:w-auto">
-            <Page  {...item} />
-            </div>
-          ))}
-        </div>
+        <MenuCard {...item}/>
       </div>
+    ))}
+  </div>
+</div>
+
 
       <div className="flex justify-end">
         <Link href="/restaurants">
