aadb0f61e17542920297af1db572241597c6f139
onlyeugene <102293184+onlyeugene@users.noreply.github.com>
2024-11-21 11:41:58 +0100

    feat: restructured folders and designs

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,34 +1,34 @@
-// // import { NextResponse } from "next/server";
-// // import axios from "axios";
+import { NextResponse } from "next/server";
+import axios from "axios";
 
-// // export async function POST(request: Request) {
-// //   try {
-// //     const body = await request.json();
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
 
-// //     const response = await axios.post(
-// //       "http://localhost:3001/api/users/login",
-// //       body,
-// //       {
-// //         headers: {
-// //           "Content-Type": "application/json",
-// //         },
-// //       }
-// //     );
+    const response = await axios.post(
+      "http://localhost:3001/api/users/login",
+      body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-// //     const data = response.data;
-// //     return NextResponse.json(data);
-// //   } catch (error: unknown) {
-// //     // console.error("Registration error:", error);
-// //     return NextResponse.json(
-// //       {
-// //         error: "Failed to process registration",
-// //       },
-// //       {
-// //         status: 500,
-// //       }
-// //     );
-// //   }
-// // }
+    const data = response.data;
+    return NextResponse.json(data);
+  } catch (error: unknown) {
+    // console.error("Registration error:", error);
+    return NextResponse.json(
+      {
+        error: "Failed to process registration",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
 
 
 
