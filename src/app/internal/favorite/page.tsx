dbb5d7b0afcfc8aede8ec13463ef5dca6cb8cb6b
Chioma <okekelinda9991@gmail.com>
2024-11-21 17:13:05 -0800

    made some changes

--- src/app/internal/favorite/page.tsx
+++ src/app/internal/favorite/page.tsx
@@ -83,10 +83,6 @@ const FavoritesPage = () => {
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
                 className={`cursor-pointer transition-colors duration-300 absolute top-4 right-3 fill-orange-500`}
               >
                 <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 1 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
@@ -140,11 +136,7 @@ const FavoritesPage = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                viewBox="0 0 24 24"  
                 className={`cursor-pointer transition-colors duration-300 absolute top-4 right-3 fill-orange-500`}
               >
                 <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 1 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
